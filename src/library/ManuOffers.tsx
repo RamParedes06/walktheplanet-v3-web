3cbf01e5f6e19e136d80dac2f6fa6eec2d027840
Ram Paredes <139292846+RamParedes06@users.noreply.github.com>
2025-04-02 14:07:42 +0800

    Update ManuOffers.tsx

--- src/library/ManuOffers.tsx
+++ src/library/ManuOffers.tsx
@@ -1,23 +1,23 @@
 export const ManuOffers = [
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%201.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573974/Corporate_Business_Manufacturing_1_wkdfma.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%202.webp&version_id=null", // 1
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573974/Corporate_Business_Manufacturing_3_vyzc74.webp", // 1
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%2010.webp&version_id=null", //2
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573974/Corporate_Business_Manufacturing_10_oudnsw.webp", //2
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%203.webp&version_id=null", //3
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573974/Corporate_Business_Manufacturing_2_tm8jbw.webp", //3
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%209.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573974/Corporate_Business_Manufacturing_4_zmcwqz.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%206.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573975/Corporate_Business_Manufacturing_7_dflfkd.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%204.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573975/Corporate_Business_Manufacturing_6_nxlvob.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%207.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573975/Corporate_Business_Manufacturing_5_deyea9.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%208.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573975/Corporate_Business_Manufacturing_9_onvpwa.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%205.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573976/Corporate_Business_Manufacturing_8_iu7sau.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2FManufacturing%2FCorporate%20%26%20Business%20Manufacturing%206.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743573974/Corporate_Business_Manufacturing_2_tm8jbw.webp",
 ];
