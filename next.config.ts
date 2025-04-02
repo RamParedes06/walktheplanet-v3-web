e7b3ae4ac7f708d7ba338ed1bb6d0a7562e71d93
Ram Paredes <139292846+RamParedes06@users.noreply.github.com>
2025-04-02 09:33:04 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,21 @@
 import type { NextConfig } from "next";
- 
+
 const nextConfig: NextConfig = {
-  /* config options here */
- 
   images: {
-    formats: ["image/avif", "image/webp", ],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '47.245.126.170',
-        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.kiwi.com',
         pathname: '/**',
       },
     ],
-    domains: ['47.245.126.170', 'images.kiwi.com'],
   },
 };
- 
+
 export default nextConfig;
- 
- 
