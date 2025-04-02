be227bf2da3b2b20e57d50b09854781d66c6a954
Ram Paredes <139292846+RamParedes06@users.noreply.github.com>
2025-04-02 14:41:42 +0800

    Update HomePartner.tsx

--- src/library/HomePartner.tsx
+++ src/library/HomePartner.tsx
@@ -1,25 +1,17 @@
 export const HomeOffers = [
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2FCard-1.webp&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-2_eetjjz.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574455/Card-1_uarkul.webp", // 1
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2FCard-2.webp&version_id=null", // 1
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-4_o1ptjp.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-3_jkfupb.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-2_eetjjz.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574455/Card-1_uarkul.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2FCard-3.webp&version_id=null", //2
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-4_o1ptjp.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-3_jkfupb.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-2_eetjjz.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574455/Card-1_uarkul.webp",
 
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2FCard-4.webp&version_id=null", //3
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid1.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid2.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid3.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid4.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid5.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid6.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid7.png&version_id=null",
-
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2FHome%2FPartners%2Fgrid8.png&version_id=null",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574456/Card-2_eetjjz.webp",
+	"https://res.cloudinary.com/dxg7sn3cy/image/upload/v1743574455/Card-1_uarkul.webp", // 1
 ];
