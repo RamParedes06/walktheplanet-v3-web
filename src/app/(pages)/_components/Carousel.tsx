3632501bd9c670ea39c42204a0bf41da860dec2d
Ram Paredes <mardie.paredes@aiqueinnovation.com>
2025-04-07 10:30:40 +0800

    fix carousel on mobile screen not showing all images

--- src/app/(pages)/_components/Carousel.tsx
+++ src/app/(pages)/_components/Carousel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Image, { StaticImageData } from "next/image";
 
@@ -9,14 +11,31 @@ const Carousel: React.FC<CarouselProps> = ({ images = [] }) => {
 	if (!Array.isArray(images) || images.length === 0) return null;
 
 	return (
-		<div className="relative w-full overflow-hidden">
-			<div className="flex animate-marquee ">
+		<div className="relative w-full overflow-hidden group">
+			<div
+				className="flex animate-marquee group-hover:[animation-play-state:paused] min-w-max"
+				style={{ animationDuration: "30s" }} // slower speed
+			>
 				{[...images, ...images].map((image, index) => (
-					<div key={index} className="flex-shrink-0">
-						<Image src={image} alt={`carousel-image-${index}`} loading="lazy" width={400} height={400} />
+					<div key={index} className="flex-shrink-0 w-[400px] h-[400px] scroll-snap-align-start">
+						<Image src={image} alt={`carousel-image-${index}`} width={400} height={400} className=" object-cover w-full h-full" loading="lazy" />
 					</div>
 				))}
 			</div>
+
+			<style jsx>{`
+				@keyframes marquee {
+					0% {
+						transform: translateX(0%);
+					}
+					100% {
+						transform: translateX(-50%);
+					}
+				}
+				.animate-marquee {
+					animation: marquee linear infinite;
+				}
+			`}</style>
 		</div>
 	);
 };
