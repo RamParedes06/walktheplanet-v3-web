1bcda16f790b466402add170b3b7d58810859874
Francis Nicholas Ramos (Intern) <francis.ramos@aiqueinnovation.com>
2025-04-14 15:30:19 +0800

    MiceEmergency section images

---
+++ src/library/MiceEmergencyImages.tsx
@@ -0,0 +1,8 @@
+export const MiceEmergencyImages = [
+  "https://res.cloudinary.com/dmxvasob7/image/upload/v1744271054/mice-emergency1_dla2yc.webp",
+  "https://res.cloudinary.com/dmxvasob7/image/upload/v1744271056/mice-emergency2_hzsgoy.webp",
+  "https://res.cloudinary.com/dmxvasob7/image/upload/v1744271054/mice-emergency3_uyr0iu.webp",
+  "https://res.cloudinary.com/dmxvasob7/image/upload/v1744271055/mice-emergency4_maby2q.webp",
+  "https://res.cloudinary.com/dmxvasob7/image/upload/v1744271053/mice-emergency5_vhznkw.webp",
+  "https://res.cloudinary.com/dmxvasob7/image/upload/v1744271053/mice-emergency6_ievqdo.webp",
+];
