2ab9f5fdd7043a5823be153df7705b1fde1e6940
Erica Moulic <erica.moullic@aiqueinnovation.com>
2025-04-14 16:01:00 +0800

    Edutourism - Emergency Section - Images

---
+++ src/library/EduEmergencyImages.tsx
@@ -0,0 +1,8 @@
+export const EduEmergencyImages = [
+    "https://res.cloudinary.com/dmxvasob7/image/upload/v1744270642/edu-emergency1_ve4jl5.webp",
+    "https://res.cloudinary.com/dmxvasob7/image/upload/v1744270641/edu-emergency2_lychp5.webp",
+    "https://res.cloudinary.com/dmxvasob7/image/upload/v1744270648/edu-emergency3_nkvass.webp",
+    "https://res.cloudinary.com/dmxvasob7/image/upload/v1744270642/edu-emergency4_hahswi.webp",
+];
+   
+  
