2b1201ee544664caaae3f54755d9a25d4785acd6
matt <matt.tan@aiqueinnovation.com>
2025-04-14 13:55:09 +0800

    Fixed: GL-2078

--- src/schema/index.tsx
+++ src/schema/index.tsx
@@ -1,20 +1,20 @@
 import { z } from "zod";
 
 const baseSchema = z.object({
-	email: z.string().email("Invalid email address").min(1, "Email is required"),
-	phoneNumber: z.string().min(1, "Phone number is required"),
-	comment: z.string().optional(),
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  phoneNumber: z.string().min(1, "Phone number is required"),
+  comment: z.string().optional(),
 });
 
 export const schemaLeisure = baseSchema.extend({
-	firstName: z.string().min(1, "First name is required"),
-	middleName: z.string().min(1, "Middle name is required"),
-	lastName: z.string().min(1, "Last name is required"),
-	travelType: z.literal("Leisure Travel"),
+  firstName: z.string().min(1, "First name is required"),
+  middleName: z.string().min(1, "Middle name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+  travelType: z.literal("Leisure Travel"),
 });
 
 export const schemaBusiness = baseSchema.extend({
-	companyName: z.string().min(1, "Company name is required"),
-	clientName: z.string().min(1, "Client name is required"),
-	travelType: z.literal("Business Travel"),
+  companyName: z.string().min(1, "Company name is required"),
+  clientName: z.string().min(1, "Client name is required"),
+  travelType: z.literal("Business Travel"),
 });
