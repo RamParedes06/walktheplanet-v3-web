757f07d71ab9f70978e691df4727fea5c4822b20
Aliyah <aliyah.sagaban@aiqueinnovation.com>
2025-04-15 17:37:41 +0800

    Add hero component in leisure

--- src/app/(pages)/leisure/page.tsx
+++ src/app/(pages)/leisure/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Hero from "../corporate/_corporate/Hero";
+import { EduImages } from "@/library/EduHero";
 
 const LeisurePage = () => {
   return (
-    <div>LeisurePage</div>
-  )
-}
+    <div>
+      {" "}
+      <Hero
+        images={EduImages}
+        title="LEISURE"
+        description={
+          <div className="flex flex-col gap-3">
+            <p>
+              Whether you crave sun-drenched beaches, cultural adventures, or
+              tranquil retreats, your{" "}
+              <span className="text-[#7EE7FC]">
+                {" "}
+                perfect escape starts here.
+              </span>
+            </p>
+          </div>
+        }
+      />
+    </div>
+  );
+};
 
-export default LeisurePage
+export default LeisurePage;
