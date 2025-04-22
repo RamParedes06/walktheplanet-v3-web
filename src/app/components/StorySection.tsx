4a17dbc26da64ec83119c80ca7261de3dc96b18d
matt <matt.tan@aiqueinnovation.com>
2025-04-22 17:54:50 +0800

    update about us page
    - CoreValues
    - StorySection
    - WhoWeAre

--- src/app/components/StorySection.tsx
+++ src/app/components/StorySection.tsx
@@ -4,11 +4,11 @@ const StorySection = () => {
   return (
     <div className="w-full py-8 md:py-16 bg-white text-gray-800">
       {/* OUR STORY */}
-      <div className="flex flex-col items-center text-center md:text-left md:items-start md:flex-row md:gap-12 px-6 py-8 md:py-[112px] md:px-[80px]">
+      <div className="flex flex-col items-center text-center md:text-left md:items-center md:flex-row md:gap-12 px-6 py-8 md:h-[396px] md:px-[80px]">
         <h2 className="text-[40px] md:text-4xl font-bold md:font-extrabold text-[#00537F] mb-4 md:mb-0 md:w-1/3">
           OUR STORY
         </h2>
-        <p className="text-base md:text-[32px] leading-relaxed md:w-4/6">
+        <p className="text-base md:text-[32px] leading-relaxed md:w-4/6 md:px-10">
           Since 2022, we&apos;ve{" "}
           <span className="text-orange-500 font-semibold">
             thoughtfully crafted unique journeys
@@ -17,9 +17,8 @@ const StorySection = () => {
           experiences in the Philippines and worldwide.
         </p>
       </div>
-
       {/* OUR MISSION */}
-      <div className="transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#00537F] hover:to-[#297A55] hover:text-white bg-white text-gray-800 py-8 px-6 text-center md:text-left md:py-[112px] md:px-[80px]">
+      <div className="border-b-2 border-t-2 border-[#E0E0E0] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#00537F] hover:to-[#297A55] hover:text-white bg-white text-gray-800 py-8 px-6 text-center md:text-left md:py-[112px] md:px-[80px]">
         <div className="flex flex-col items-center md:items-start md:flex-row gap-2 md:gap-6">
           <div className="text-xl md:text-3xl font-semibold mb-2 md:mb-0 md:w-3/6">
             01
@@ -33,9 +32,8 @@ const StorySection = () => {
           </div>
         </div>
       </div>
-
       {/* OUR VISION */}
-      <div className="transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#00537F] hover:to-[#297A55] hover:text-white bg-white text-gray-800 py-8 px-6 text-center md:text-left md:py-[112px] md:px-[80px]">
+      <div className="border-b-2 border-[#E0E0E0] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-[#00537F] hover:to-[#297A55] hover:text-white bg-white text-gray-800 py-8 px-6 text-center md:text-left md:py-[112px] md:px-[80px]">
         <div className="flex flex-col items-center md:items-start md:flex-row gap-2 md:gap-6">
           <div className="text-xl md:text-3xl font-semibold mb-2 md:mb-0 md:w-3/6">
             02
--- src/app/components/WhoWeAre.tsx
+++ src/app/components/WhoWeAre.tsx
@@ -3,10 +3,11 @@ import image01 from "@/assets/images/about/Image 01.png";
 import image02 from "@/assets/images/about/Image 02.png";
 import image03 from "@/assets/images/about/Image 03.png";
 import bg from "@/assets/images/about/BGIllustration.svg";
+import bgMobile from "@/assets/images/about/g10.png";
 
 export default function WhoWeAreSection() {
   return (
-    <div className="flex flex-col w-full max-w-full mx-auto pt-10 md:pt-20 bg-white">
+    <div className="flex flex-col w-full max-w-full mx-auto pt-5 md:pt-10 bg-white">
       <div className="relative bg-white">
         {/* Top Images */}
         <div className="flex w-full h-[500px] md:h-[500px] gap-1">
@@ -59,24 +60,22 @@ export default function WhoWeAreSection() {
 
       {/* Description Section */}
       <div className="relative py-[64px] md:py-[112px] px-4 md:px-8 bg-white">
-        <div className="max-w-4xl mx-auto text-start md:text-center space-y-4">
-          <p className="text-[16px] md:text-xl text-gray-800 leading-relaxed">
+        <div className="max-w-4xl mx-auto text-start md:text-center">
+          <p className="text-[16px] md:text-[24px] text-gray-800 leading-relaxed mb-4">
             We&apos;re a Filipino-founded startup that{" "}
             <span className="font-semibold">
-              understands every Juan&apos;s desire
-              <br />
-              for effortless, exciting, and accessible travel experiences.
+              understands every Juan&apos;s desire for effortless, exciting, and
+              accessible travel experiences.
             </span>
           </p>
 
-          <p className="text-[16px] md:text-xl text-gray-800 leading-relaxed">
+          <p className="text-[16px] md:text-[24px] text-gray-800 leading-relaxed">
             That&apos;s why we expertly curate journeys that match your pace,
-            style, and budget,
-            <br />
-            from spontaneous weekend outings to once-in-a-lifetime adventures.
+            style, and budget, from spontaneous weekend outings to
+            once-in-a-lifetime adventures.
           </p>
 
-          <p className="text-[16px] md:text-xl text-gray-800 leading-relaxed">
+          <p className="text-[16px] md:text-[24px] text-gray-800 leading-relaxed">
             <span className="font-semibold">
               Wherever you dream of going, WalkThePlanet takes you there.
             </span>
@@ -84,12 +83,19 @@ export default function WhoWeAreSection() {
         </div>
 
         {/* Background Illustration */}
-        <div className="absolute bottom-0 right-0  w-full h-100 md:w-1/2 md:h-5/5 pointer-events-none opacity-20">
+        <div className="absolute bottom-0 right-0 w-full h-full md:w-full md:h-full pointer-events-none opacity-20">
           <Image
             src={bg}
             alt="Travel-themed background illustration"
             fill
-            className="md:object-contain"
+            className="md:object-right md:object-contain hidden md:block"
+          />
+
+          <Image
+            src={bgMobile}
+            alt="Travel-themed background illustration"
+            fill
+            className="object-right object-cover md:hidden"
           />
         </div>
       </div>
