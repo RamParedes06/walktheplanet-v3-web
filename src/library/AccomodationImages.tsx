f9c957e70934394609878ade513a0c6cdf384034
Aliyah <aliyah.sagaban@aiqueinnovation.com>
2025-04-24 15:51:10 +0800

    Update assets

--- src/library/AccomodationImages.tsx
+++ src/library/AccomodationImages.tsx
@@ -8,30 +8,25 @@ export const AccomodationImages: AccomodationProps[] = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206457/Accomodation_ktmrub.webp",
+      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745480932/Accom1_dkxa3f.webp",
     label: "Hotels and Resorts",
   },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206456/Inns_and_Staycations_xazusm.webp",
+      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745480931/Accom2_zs31wj.webp",
     label: "Inns and Staycations",
   },
   {
     id: 3,
     image:
-      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206455/Residences_z4rr2m.webp",
+      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745480931/Accom3_hmiyt0.webp",
     label: "Residences",
   },
   {
     id: 4,
     image:
-      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206454/Boutique_Hotels_c5kg7p.webp",
+      "https://res.cloudinary.com/dmxvasob7/image/upload/v1745480930/Accom4_tvjn2z.webp",
     label: "Boutique Hotels"
   },
 ];
-
-//"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206457/Accomodation_ktmrub.webp",
-//"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206456/Inns_and_Staycations_xazusm.webp",
-//"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206455/Residences_z4rr2m.webp",
-//"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206454/Boutique_Hotels_c5kg7p.webp",
