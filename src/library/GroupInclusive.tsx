dddf48a82ea5e53e84b285a4b75cc2527e4fae72
Ram Paredes <mardie.paredes@aiqueinnovation.com>
2025-04-24 15:09:23 +0800

    fix iamges

--- src/library/GroupInclusive.tsx
+++ src/library/GroupInclusive.tsx
@@ -1,8 +1,26 @@
 export const GroupImages = [
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206714/Image_kyozrp.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206721/Image-1_gzzbjt.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206721/Image-2_rurm5i.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206723/Image-3_ua1hrq.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206718/Image-4_oav8vq.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206719/Image-5_qqtboo.webp",
+	{
+		url: "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206714/Image_kyozrp.webp",
+		title: "",
+	},
+	{
+		url: "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206721/Image-1_gzzbjt.webp",
+		title: "",
+	},
+	{
+		url: "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206721/Image-2_rurm5i.webp",
+		title: "",
+	},
+	{
+		url: "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206723/Image-3_ua1hrq.webp",
+		title: "",
+	},
+	{
+		url: "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206718/Image-4_oav8vq.webp",
+		title: "",
+	},
+	{
+		url: "https://res.cloudinary.com/dmxvasob7/image/upload/v1745206719/Image-5_qqtboo.webp",
+		title: "",
+	},
 ];
