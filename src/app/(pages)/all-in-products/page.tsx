ac71bcb9c52fca6439469e25264639f8d12d125f
Aliyah <aliyah.sagaban@aiqueinnovation.com>
2025-04-24 11:42:54 +0800

    Finished Call To Action in Products Page

--- src/app/(pages)/all-in-products/page.tsx
+++ src/app/(pages)/all-in-products/page.tsx
@@ -8,6 +8,16 @@ import { TranspoImages } from "@/library/TransportationImages";
 import Cruise from "./_components/Cruise";
 import Accomodations from "./_components/Accomodations";
 import Tours from "../leisure/_components/Tours";
+import CallToAction from "../corporate/_corporate/CallToAction";
+import {
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+} from "@/library/AllInProductsCTA";
+import Footer from "../_components/Footer";
 
 function AllInProducts() {
   return (
@@ -45,6 +55,23 @@ function AllInProducts() {
 
         {/* Section 9 - Travel Essentials  */}
         <Tours productsVersion />
+
+		{/* Section 10 - Merchandise  */}
+
+        {/* Section 11 - Call to Action  */}
+        <CallToAction
+          image1={image4}
+          image2={image5}
+          image3={image6}
+          image4={image1}
+          image5={image2}
+          image6={image3}
+          title="Got any travel plans in mind? We’re just one click away!"
+          buttonLabel="Shop your travel needs here!"
+        />
+
+        {/* Footer  */}
+        <Footer />
       </SmoothScroll>
     </div>
   );
