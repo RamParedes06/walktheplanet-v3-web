7b7e0d8e2f3f52831d2cb38b03b8e0ff8b33c2bf
Aliyah <aliyah.sagaban@aiqueinnovation.com>
2025-04-24 17:51:58 +0800

    Fix CTA Mice and Corp

--- src/library/CorpoCTA.tsx
+++ src/library/CorpoCTA.tsx
@@ -1,12 +1,12 @@
 export const image1 =
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2Fcta%2FCorporate%20%26%20Business%20CTA%201.webp&version_id=null";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744774547/CTA1_n78nrk.webp";
 export const image2 =
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2Fcta%2FCorporate%20%26%20Business%20CTA%202.webp&version_id=null";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744774546/CTA2_nqg4ui.webp";
 export const image3 =
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2Fcta%2FCorporate%20%26%20Business%20CTA%203.webp&version_id=null";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744774546/CTA3_qhyegu.webp";
 export const image4 =
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2Fcta%2FCorporate%20%26%20Business%20CTA%204.webp&version_id=null";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744774547/CTA4_pjsguj.webp";
 export const image5 =
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2Fcta%2FCorporate%20%26%20Business%20CTA%205.webp&version_id=null";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744774547/CTA5_tpsgdv.webp";
 export const image6 =
-	"http://47.245.126.170:30085/api/v1/buckets/walktheplanet-assets/objects/download?preview=true&prefix=wtp-landing-page%2Fcorporate%2Fcta%2FCorporate%20%26%20Business%20CTA%206.webp&version_id=null";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744774548/CTA6_l3nxia.webp";
--- src/library/MiceCTA.tsx
+++ src/library/MiceCTA.tsx
@@ -1,12 +1,12 @@
 export const image1 =
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1744683935/mice-banner-4_zv51sf.webp";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744683935/mice-banner-4_zv51sf.webp";
 export const image2 =
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1744683935/mice-banner-5_gqvpiv.webp";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744683935/mice-banner-5_gqvpiv.webp";
 export const image3 =
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1744683935/mice-banner-6_fl5jbc.webp";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744683935/mice-banner-6_fl5jbc.webp";
 export const image4 =
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1744683935/mice-banner-1_xq7af2.webp";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744683935/mice-banner-1_xq7af2.webp";
 export const image5 =
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1744685628/mice-banner-2_j7sh3f.jpg";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744683935/mice-banner-2_n2r1lf.webp";
 export const image6 =
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1744685554/mice-banner-3_gwr3aw.jpg";
+	"https://res.cloudinary.com/dmxvasob7/image/upload/c_crop,ar_1:1/v1744683935/mice-banner-3_gf4awn.webp";
