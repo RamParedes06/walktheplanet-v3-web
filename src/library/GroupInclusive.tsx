fc159e6fd57b5531299cd9d7d05cdd17f0bb0056
Aliyah <aliyah.sagaban@aiqueinnovation.com>
2025-04-24 15:33:48 +0800

    Update assets in All in Products

--- src/library/GroupInclusive.tsx
+++ src/library/GroupInclusive.tsx
@@ -1,8 +1,8 @@
 export const GroupImages = [
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206714/Image_kyozrp.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206721/Image-1_gzzbjt.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206721/Image-2_rurm5i.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206723/Image-3_ua1hrq.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206718/Image-4_oav8vq.webp",
-	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745206719/Image-5_qqtboo.webp",
+	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745479610/Group1_fdj5vr.webp",
+	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745479613/Group2_qrfyhm.webp",
+	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745479618/Group3_uf8ku3.webp",
+	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745479605/Group4_ijustf.webp",
+	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745479607/Group5_knr0bl.webp",
+	"https://res.cloudinary.com/dmxvasob7/image/upload/v1745479615/Group6_ofw6zx.webp",
 ];
