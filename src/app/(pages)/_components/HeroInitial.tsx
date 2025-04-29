4d187e4e7767eb1ab4b7745c18cf9062aee41752
Aliyah <aliyah.sagaban@aiqueinnovation.com>
2025-04-29 13:44:41 +0800

    Fix bug on our beliefs section

--- src/app/(pages)/_components/HeroInitial.tsx
+++ src/app/(pages)/_components/HeroInitial.tsx
@@ -386,7 +386,7 @@ export default function Hero() {
       {/* Menu Desktop View */}
       <div className="hero-container bg-white relative" ref={heroContainerRef}>
         {/* tabs and logos  */}
-        <div className="absolute lg:left-[5%] lg:bottom-[20%] bottom-[10%] w-full lg:w-max z-50 flex flex-col lg:flex-row justify-between items-center gap-4 mt-1 ">
+        <div className="absolute lg:left-[5%] lg:bottom-[20%] bottom-[10%] z-10 w-full lg:w-max flex flex-col lg:flex-row justify-between items-center gap-4 mt-1 ">
           <div
             className="flex flex-wrap justify-center lg:justify-start gap-2.5 lg:gap-4 lg:max-w-[850px] max-w-[350px] relative"
             ref={tabsContainerRef}
