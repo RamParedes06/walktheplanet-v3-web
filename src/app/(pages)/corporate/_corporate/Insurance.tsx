ad6872fa6f0bfc32c140c578227b18d413985f02
matt <matt.tan@aiqueinnovation.com>
2025-05-16 11:14:23 +0800

    Fixed: GL-2236
    - remove unused import

--- src/app/(pages)/corporate/_corporate/Insurance.tsx
+++ src/app/(pages)/corporate/_corporate/Insurance.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Image from 'next/image'
 
-import illustration from '@/assets/svg/BGIllustration.svg'
 import TextReveal from '@/app/(pages)/_components/TextReveal'
 import Carousel from '@/app/(pages)/_components/Carousel'
 
