4af47fba16777695dd2bf7c02b285b75776535e1
matt <matt.tan@aiqueinnovation.com>
2025-05-20 10:45:54 +0800

    Fixed: GL-2201

--- src/app/(pages)/travel-stories/page.tsx
+++ src/app/(pages)/travel-stories/page.tsx
@@ -1,38 +1,43 @@
-import React from "react";
-import Testimonials from "./_components/Testimonials";
-import CallToAction from "../corporate/_corporate/CallToAction";
-import {
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-} from "@/library/CorpoCTA";
-import Footer from "../_components/Footer";
-import Hero from "../corporate/_corporate/Hero";
-import { TravelStoriesHero } from "@/library/TravelStoriesHero";
+import React from 'react'
+import Testimonials from './_components/Testimonials'
+import CallToAction from '../corporate/_corporate/CallToAction'
+import { image1, image2, image3, image4, image5, image6 } from '@/library/CorpoCTA'
+import Footer from '../_components/Footer'
+import Hero from '../corporate/_corporate/Hero'
+import { TravelStoriesHero, TravelStoriesHeroMobile } from '@/library/TravelStoriesHero'
 
 const page = () => {
   return (
     <>
-      <Hero
-        gradient="bg-gradient-to-r from-[#14476F] via-[#00A9CE] to-[#FFC000]"
-        images={TravelStoriesHero}
-        title="TRAVEL STORIES"
-        description={
-          <div className="lg:space-y-10 space-y-5">
-            <p className="font-normal lg:text-[24px] text-base">
-              From dreamy getaways to epic adventures, we make travel dreams
-              come true.
-            </p>
-            <p className="font-normal lg:text-[24px] text-base">
-              See why our clients can&apos;t stop raving about their
-              unforgettable journeys with WalkThePlanet!
-            </p>
-          </div>
-        }
-      />
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <Hero
+          gradient="bg-gradient-to-r from-[#14476F] via-[#00A9CE] to-[#FFC000]"
+          images={TravelStoriesHero}
+          title="TRAVEL STORIES"
+          description={
+            <div className="lg:space-y-10 space-y-5">
+              <p className="font-normal lg:text-[24px] text-base">From dreamy getaways to epic adventures, we make travel dreams come true.</p>
+              <p className="font-normal lg:text-[24px] text-base">See why our clients can&apos;t stop raving about their unforgettable journeys with WalkThePlanet!</p>
+            </div>
+          }
+        />
+      </div>
+      {/* Mobile */}
+      <div className="lg:hidden block">
+        <Hero
+          gradient="bg-gradient-to-r from-[#14476F] via-[#00A9CE] to-[#FFC000]"
+          images={TravelStoriesHeroMobile}
+          title="TRAVEL STORIES"
+          description={
+            <div className="lg:space-y-10 space-y-5">
+              <p className="font-normal lg:text-[24px] text-base">From dreamy getaways to epic adventures, we make travel dreams come true.</p>
+              <p className="font-normal lg:text-[24px] text-base">See why our clients can&apos;t stop raving about their unforgettable journeys with WalkThePlanet!</p>
+            </div>
+          }
+        />
+      </div>
+
       <Testimonials />
       <CallToAction
         url="/contact-us"
@@ -47,7 +52,7 @@ const page = () => {
       />
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default page;
+export default page
