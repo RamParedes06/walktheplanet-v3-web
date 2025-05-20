cdd9d1897061d442759b8dff7e3a661ccd9a413a
matt <matt.tan@aiqueinnovation.com>
2025-05-20 10:18:05 +0800

    Fixed: GL-2240

--- src/app/(pages)/leisure/page.tsx
+++ src/app/(pages)/leisure/page.tsx
@@ -1,33 +1,46 @@
-import React from "react";
-import Hero from "../corporate/_corporate/Hero";
-import { LeisureImages } from "@/library/LeisureHero";
-import Footer from "../_components/Footer";
-import OnlineBooking from "./_components/OnlineBooking";
-import LiveSelling from "./_components/LiveSelling";
-import Tours from "./_components/Tours";
+import React from 'react'
+import Hero from '../corporate/_corporate/Hero'
+import { LeisureImages, LeisureImagesMobile } from '@/library/LeisureHero'
+import Footer from '../_components/Footer'
+import OnlineBooking from './_components/OnlineBooking'
+import LiveSelling from './_components/LiveSelling'
+import Tours from './_components/Tours'
 
 const LeisurePage = () => {
   return (
     <div>
-      {" "}
       {/* Section 1 - Hero  */}
-      <Hero
-        images={LeisureImages}
-        title="LEISURE"
-        description={
-          <div className="flex flex-col gap-3">
-            <p>
-              Whether you crave sun-drenched beaches, cultural adventures, or
-              tranquil retreats, your{" "}
-              <span className="text-[#7EE7FC]">
-                {" "}
-                perfect escape starts here.
-              </span>
-            </p>
-          </div>
-        }
-        gradient="bg-gradient-to-r from-[#FAAE19] via-[#198A6F] to-[#14476F]"
-      />
+      {/* Desktop */}
+      <div className="hidden lg:block">
+        <Hero
+          images={LeisureImages}
+          title="LEISURE"
+          description={
+            <div className="flex flex-col gap-3">
+              <p>
+                Whether you crave sun-drenched beaches, cultural adventures, or tranquil retreats, your <span className="text-[#7EE7FC]"> perfect escape starts here.</span>
+              </p>
+            </div>
+          }
+          gradient="bg-gradient-to-r from-[#FAAE19] via-[#198A6F] to-[#14476F]"
+        />
+      </div>
+
+      <div className="lg:hidden">
+        <Hero
+          images={LeisureImagesMobile}
+          title="LEISURE"
+          description={
+            <div className="flex flex-col gap-3">
+              <p>
+                Whether you crave sun-drenched beaches, cultural adventures, or tranquil retreats, your <span className="text-[#7EE7FC]"> perfect escape starts here.</span>
+              </p>
+            </div>
+          }
+          gradient="bg-gradient-to-r from-[#FAAE19] via-[#198A6F] to-[#14476F]"
+        />
+      </div>
+      {/* Mobile */}
       {/* Section 2 - Online booking  */}
       <OnlineBooking />
       {/* Section 3 - Tiktok Live Selling  */}
@@ -37,7 +50,7 @@ const LeisurePage = () => {
       {/* Section 5 - Footer  */}
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default LeisurePage;
+export default LeisurePage
